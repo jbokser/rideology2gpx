513fbaa0a09c0ffbb4cc935ac5111677e032a332
Juan S. Bokser <juan.bokser@gmail.com>
2024-02-23 15:11:48 -0300

    feat: cli improvements

--- rideology2gpx_tool/cli.py
+++ rideology2gpx_tool/cli.py
@@ -1,7 +1,8 @@
 from click import command, option, argument, Choice
 from click import Path as TypePath
 from click import DateTime as TypeDateTime
-from click.exceptions import UsageError
+from click import IntRange as TypeIntRange
+from click.exceptions import UsageError, BadParameter
 from datetime import datetime
 from pathlib import Path
 from tabulate import tabulate
@@ -23,58 +24,31 @@ def dinamic_help_decorator(**wildcards):
         return f
     return fnc
 
-filter_options = [
-        'from-0-to-100-kmh',
-        'from-20-to-120-kmh',
-        'from-100-to-200-kmh',
-        'from-100-to-top-speed',
-        'from-120-to-top-speed',
-        'from-0-to-top-speed',
-        'from-60-to-top-speed',
-        'chop-1km-at-end',
-        'chop-1km-at-start',
-        'chop-1km-at-start-and-end',
-        'chop-3km-at-end',
-        'chop-3km-at-start',
-        'chop-3km-at-start-and-end'
-    ]
-def filter_options_str():
-    opts = filter_options[:]
-    opts_row = ['']
-    opts_table = []
-    while opts:
-        opts_row.append(opts.pop(0))
-        if len(opts_row)==4:
-            opts_table.append(opts_row)
-            opts_row = ['']
-    if opts_row:
-        opts_table.append(opts_row)
-    return tabulate(opts_table, tablefmt='plain')
 
-epilog = f"""\b
-The valid values ​for FILTER are:
-{filter_options_str()}
-"""
-
-@command(context_settings=dict(help_option_names=['-h', '--help']),
-         epilog=epilog)
+@command(context_settings=dict(help_option_names=['-h', '--help']))
 @option('-v', '--version', 'show_version', is_flag=True,
     help='Show version and exit.')
 @option('-d', '--date', 'start_time',
         type=TypeDateTime(formats=dt_formats),
         default=dt_str_default,
         help='Starting datetime for the track in the GPX files.')
-@option('-f', '--filter', 'data_filter',
-        type=Choice(filter_options, case_sensitive=False),
-        metavar='FILTER',
-        help='Filter applied to waypoints.')
 @argument('csv_file', type=TypePath(), required=False)
 @argument('output_dir', type=TypePath(), required=False)
 @option('-g', '--graph', 'graph', is_flag=True,
     help='Make graphs and md report.')
+@option('-s', '--starting-chop', 'starting_chop',
+        type=TypeIntRange(0, 10), default=0,
+        help='Cut the first X kilometres.', metavar="X")
+@option('-e', '--ending-chop', 'ending_chop',
+        type=TypeIntRange(0, 10), default=0,
+        help='Cut the last X kilometres.', metavar="X")
+@option('-a', '--acceleration', 'acceleration',
+        type=(str, str), metavar="MIN_SPEED MAX_SPEED",
+        help="Filters waypoints to only what is included between the speeds.")
 @dinamic_help_decorator(**app_info)
 def cli(csv_file, output_dir, start_time,
-        show_version=False, data_filter=None, graph=None):
+        show_version=False, graph=None, ending_chop=0,
+        starting_chop=0, acceleration=None):
     """
     A simple command line program to transform log files obtained with the
     Kawasaki Rideology App into GPX files.
@@ -104,75 +78,55 @@ def cli(csv_file, output_dir, start_time,
     
     min_speed = None
     max_speed = None
-    ending_chop = 0
-    starting_chop = 0
+    
+    if acceleration is not None:
+
+        min_speed, max_speed = [
+            str(x).lower().strip() for x in list(acceleration)]
+        
+        try:
+            min_speed = int(min_speed)
+        except ValueError:
+            raise BadParameter('MIN_SPEED must be integer')
+        if min_speed < 0:
+            raise BadParameter('MIN_SPEED < 0')
+        if min_speed > 400:
+            raise BadParameter('MIN_SPEED > 400')
+
+        try:
+            max_speed = int(max_speed)
+        except ValueError:
+            if max_speed in ['top', 'max']:
+                max_speed = None
+            else:
+                raise BadParameter(
+                    "MAX_SPEED must be integer or the 'top' wildcard")
+        if max_speed is not None:
+            if max_speed < 0:
+                raise BadParameter('MAX_SPEED < 0')
+            if max_speed > 400:
+                raise BadParameter('MAX_SPEED > 400')
+
     subtitle = ''
-    out_filename_suffix = ''
+    out_filename_suffix = []
 
-    if data_filter == 'from-0-to-100-kmh':
-        min_speed = 0
-        max_speed = 100
-        subtitle = 'from stop to 100 km/h'
-        out_filename_suffix='(0-100kmh)'
+    if min_speed is not None:
 
-    elif data_filter == 'from-20-to-120-kmh':
-        min_speed = 20
-        max_speed = 120
-        subtitle = 'from 20 km/h to 100 km/h'
-        out_filename_suffix='(20-120kmh)'
+        str_min_speed = f"{min_speed} km/h" if min_speed else 'stop'
+        str_max_speed = f"{max_speed} km/h" if max_speed else 'top speed'
 
-    elif data_filter == 'from-100-to-200-kmh':
-        min_speed = 100
-        max_speed = 200
-        subtitle = 'from 100 km/h to 200 km/h'
-        out_filename_suffix='(100-200kmh)'
+        subtitle = f"from {str_min_speed} to {str_max_speed}"
 
-    elif data_filter == 'from-0-to-top-speed':
-        min_speed = 0
-        subtitle = 'from stop to top speed'
-        out_filename_suffix='(0-top-kmh)'
+        str_min_speed = f"{min_speed}"
+        str_max_speed = f"{max_speed}kmh" if max_speed else 'top-kmh'
+        
+        out_filename_suffix.append(f"{str_min_speed}-{str_max_speed}")   
+    
+    if ending_chop or starting_chop:
+        out_filename_suffix.append('chop')
 
-    elif data_filter == 'from-60-to-top-speed':
-        min_speed = 60
-        subtitle = 'from 60 km/h to top speed'
-        out_filename_suffix='(60-top-kmh)'
-
-    elif data_filter == 'from-100-to-top-speed':
-        min_speed = 100
-        subtitle = 'from 100 km/h to top speed'
-        out_filename_suffix='(100-top-kmh)'
-
-    elif data_filter == 'from-120-to-top-speed':
-        min_speed = 120
-        subtitle = 'from 120 km/h to top speed'
-        out_filename_suffix='(120-top-kmh)'
-
-    elif data_filter == 'chop-1km-at-end':
-        ending_chop = 1
-        out_filename_suffix='chop'
-
-    elif data_filter == 'chop-1km-at-start':
-        starting_chop = 1
-        out_filename_suffix='chop'
-
-    elif data_filter == 'chop-1km-at-start-and-end':
-        ending_chop = 1
-        starting_chop = 1
-        out_filename_suffix='chop'
-
-    elif data_filter == 'chop-3km-at-end':
-        ending_chop = 3
-        out_filename_suffix='chop'
-
-    elif data_filter == 'chop-1km-at-start':
-        starting_chop = 3
-        out_filename_suffix='chop'
-
-    elif data_filter == 'chop-3km-at-start-and-end':
-        ending_chop = 3
-        starting_chop = 3
-        out_filename_suffix='chop'
-
+    out_filename_suffix = '(' + '-'.join(out_filename_suffix) + ')'
+    
     main(
         csv_file,
         output_dir = output_dir,
