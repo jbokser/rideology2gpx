beb272bfc24c714c1a389813de4bb8efc56f65ca
Juan S. Bokser <juan.bokser@gmail.com>
2024-03-28 11:09:55 -0300

    fix: this makes a lot of noise

--- rideology2gpx_tool/__init__.py
+++
@@ -1,5 +0,0 @@
-from .app_info import version, author, author_email, description, app_name, repo_url
-from .data_file import DataFile
-from .gpx_file import GpxFile
-from .main import main
-from .cli import cli
